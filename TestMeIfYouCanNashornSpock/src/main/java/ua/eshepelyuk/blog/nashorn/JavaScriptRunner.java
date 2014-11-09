827f5d18904cccd3a5829c9b88ed9591209a96d8
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2014-11-09 13:59:20 +0200

    rearrangng source code

--- TestMeIfYouCanNashornSpock/src/main/java/ua/eshepelyuk/blog/nashorn/JavaScriptRunner.java
+++ TestMeIfYouCanNashornSpock/src/main/java/ua/eshepelyuk/blog/nashorn/JavaScriptRunner.java
@@ -1,7 +1,5 @@
 package ua.eshepelyuk.blog.nashorn;
 
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -9,14 +7,10 @@ import java.io.InputStreamReader;
 import java.util.Map;
 
 public class JavaScriptRunner {
-    public static Object run(String script, Map<String, Object> params) throws Exception {
-        ScriptEngineManager factory = new ScriptEngineManager();
-        ScriptEngine engine = factory.getEngineByName("nashorn");
-
-        for (Map.Entry<String, Object> entry : params.entrySet()) {
-            engine.getBindings(ScriptContext.ENGINE_SCOPE).put(entry.getKey(), entry.getValue());
-        }
-
-        return engine.eval(new InputStreamReader(JavaScriptRunner.class.getResourceAsStream(script)));
-    }
+  public static Object run(String script, Map<String, Object> params) throws Exception {
+    ScriptEngineManager factory = new ScriptEngineManager();
+    ScriptEngine engine = factory.getEngineByName("nashorn");
+    engine.getBindings(ScriptContext.ENGINE_SCOPE).putAll(params);
+    return engine.eval(new InputStreamReader(JavaScriptRunner.class.getResourceAsStream(script)));
+  }
 }
--- TestMeIfYouCanNashornSpock/src/test/groovy/ua/eshepelyuk/blog/nashorn/Jasmine2Specification.groovy
+++ TestMeIfYouCanNashornSpock/src/test/groovy/ua/eshepelyuk/blog/nashorn/Jasmine2Specification.groovy
@@ -5,21 +5,29 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class Jasmine2Specification extends Specification {
+  @Shared def jasmineResults
 
-    @Shared def jasmineReport
+  def setupSpec() {
+    def scriptParams = [
+        "__jasmineSpec__"    : getMetaClass().getMetaProperty("SPEC").getProperty(null),
+        "__jasmineSpecName__": "${this.class.simpleName}.groovy",
+        "__scriptBaseDir__"  : Jasmine2Specification.class.getResource("/jasmine-2.0.2").toExternalForm()
+    ]
+    jasmineResults = JavaScriptRunner.run("/jasmine-runner.js", scriptParams)
+  }
 
-    def setupSpec() {
-        def specText = getMetaClass().getMetaProperty("SPEC").getProperty(null)
-        jasmineReport = JavaScriptRunner.run("/jasmine-runner.js"
-                , ["__jasmineSpec__": specText, "__jasmineSpecName__": "${this.class.simpleName}.groovy",
-                   "__scriptBaseDir__": Jasmine2Specification.class.getResource("/jasmine-2.0.2").toExternalForm()])
-    }
+  def isPassed(def specRes) {specRes.status == "passed" || specRes.status == "pending"}
 
-    @Unroll def '#specName'() {
-        expect:
-        assert item.status == "passed" || item.status == "pending", item.failedExpectations.collect {it.value}.collect{it.stack}.join("\n\n\n")
-        where:
-        item << jasmineReport.collect { it.value }
-        specName =(item.status != "pending" ? item.fullName : "IGNORED: $item.fullName")
-    }
+  def specErrorMsg(def specResult) {
+    item.failedExpectations.collect { it.value }
+      .collect {it.stack}.join("\n\n\n")
+  }
+
+  @Unroll def '#specName'() {
+    expect:
+      assert isPassed(item), specErrorMsg(item)
+    where:
+      item << jasmineResults.collect { it.value }
+      specName = (item.status != "pending" ? item.fullName : "IGNORED: $item.fullName")
+  }
 }

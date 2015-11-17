8f1ce753d9c35377c1c64de3cb61f13963fd1240
Ievgenii.Shepeliuk <I.Shepeliuk@external.oberthur.com>
2015-11-17 18:42:20 +0200

    + code and tests are written + gradle update

--- TestMeIfUCanAkkaSpock/src/main/java/ua/eshepelyuk/blog/GreetExtensionActor.java
+++ TestMeIfUCanAkkaSpock/src/main/java/ua/eshepelyuk/blog/GreetExtensionActor.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class GreetExtensionActor extends UntypedActor {
     @Override
     public void onReceive(Object message) throws Exception {
-        GreetExtension greetExtension = GreetExtension.KEY.apply(context().system());
+        GreetExtension greetExtension = GreetExtension.KEY.get(context().system());
         sender().tell(greetExtension.greetWord() + " " + Objects.toString(message), self());
     }
 }
